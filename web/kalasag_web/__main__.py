b9eb13704680c22fbe085da0102b78b1bc8b3e8c
Beato Bongco <beatobongco@gmail.com>
2014-11-03 18:29:11 +0800

    Port changes and yml

--- web/kalasag_web/__main__.py
+++ web/kalasag_web/__main__.py
@@ -3,4 +3,4 @@ from kalasag_web.app import create_app
 
 if __name__ == "__main__":
   app = create_app(DEBUG=True)
-  app.run(host="0.0.0.0", threaded=True)
+  app.run(host="0.0.0.0", port=9000, threaded=True)
